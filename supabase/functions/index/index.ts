f4aa9f8176b521e2ae59a7fcda67a1320a284eb1
Caruy Chen <caruychen@gmail.com>
2023-01-19 15:41:45 +0200

    update

--- supabase/functions/index/index.ts
+++ supabase/functions/index/index.ts
@@ -46,7 +46,8 @@ const queryHistoryRows = async (clientQuery: string): Promise<Response> => {
     timePeriod: timePeriod,
     readings: readings
   }
-  return new Response(JSON.stringify(response), {
+  return new Response(JSON.stringify(response),
+  {
     headers: { ...corsHeaders, "Content-Type": "application/json" },
     status: 200
   });
@@ -163,7 +164,7 @@ async function getLatest(): Promise<Response> {
 const returnError = (error: any): Response => {
   console.error(error);
   return new Response(JSON.stringify({ error: error.message }), {
-    headers: { "Content-Type": "application/json" },
+    headers: { ...corsHeaders, "Content-Type": "application/json" },
     status: 400
   })
 }
@@ -172,7 +173,10 @@ serve((req: any): Response | Promise<Response> => {
   const { method } = req;
   const url = new URL(req.url);
   const params: URLSearchParams = url.searchParams;
-
+  if (method === 'OPTIONS') {
+    const response = new Response('ok', { headers: corsHeaders })
+    return response
+  }
   try {
     switch (true) {
       case method === 'GET' && url.pathname === '/index/scales/history':
@@ -190,8 +194,8 @@ serve((req: any): Response | Promise<Response> => {
 })
 
 // To invoke:
-// curl -i --location --request GET 'http://localhost:54321/functions/v1/scales/history' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+// curl -i --location --request GET 'http://localhost:54321/functions/v1/index/scales/history' \
+//  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
 //   --data '{"method":"GET"}'
 
@@ -201,4 +205,4 @@ serve((req: any): Response | Promise<Response> => {
 //   --header 'Content-Type: application/json' \
 //   --data '{"method":"GET"}'
 
-//curl -L -X GET 'https://vfiomlqwajbenjwswajz.functions.supabase.co/index/scales/history' -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmaW9tbHF3YWpiZW5qd3N3YWp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQwMjc1MTYsImV4cCI6MTk4OTYwMzUxNn0.hvG2Wpfq3SHFq1I6SW_YJZ71ge-0y6ksEXuEjbkgnKM'
+//curl -L -X GET 'http://localhost:54321/index/scales/history' -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmaW9tbHF3YWpiZW5qd3N3YWp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQwMjc1MTYsImV4cCI6MTk4OTYwMzUxNn0.hvG2Wpfq3SHFq1I6SW_YJZ71ge-0y6ksEXuEjbkgnKM'
