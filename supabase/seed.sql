6eec4a5a3bbb2fc15ec4e136bbe510106bcad106
Caruy Chen <caruychen@gmail.com>
2023-02-28 10:55:42 +0200

    sql seed comment

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -7,3 +7,12 @@ create table
         registered  bool,
         name        text
     );
+
+/*
+insert into scales
+ (device_id, user_id, registered, name)
+values
+ (39748960246732, 'e32f5583-c101-4bac-97eb-b77fe01109f1', true, 'kitchen'),
+ (79741262426312, 'e32f5583-c101-4bac-97eb-b77fe01109f1', true, 'leftovers'),
+ (123, '24360e50-e668-44cc-8cd9-7a6e9a10f4a8', false, 'bones');
+ */
